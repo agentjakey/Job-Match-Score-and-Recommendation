516610f51eecd931a3e83157c17594d38b7bd01d
agentjakey <jgo001@ucsd.edu>
2025-08-04 16:51:35 -0700

    Update base.py
    Create template for base;
    load dataset, 
    load embedding model,
    parse resume using ResumeParser,
    Read JD,
    Compute score using cos sim,
    generate suggestions to improve resume (test dif. models or manual vague),
    exec

--- base.py
+++ base.py
@@ -1,1 +1,33 @@
+import os
+import pandas as pd
+import kagglehub
+from sentence_transformers import SentenceTransformer, util
+from pyresparser import ResumeParser
 
+# -----------------------------
+# Load Kaggle Dataset
+# -----------------------------
+
+# -----------------------------
+# Load Embedding Model
+# -----------------------------
+
+# -----------------------------
+# Parse Resume
+# -----------------------------
+
+# -----------------------------
+# Read Job Description
+# -----------------------------
+
+# -----------------------------
+# Compute Match Score
+# -----------------------------
+
+# -----------------------------
+# Generate Improvement Suggestions
+# -----------------------------
+
+# -----------------------------
+# Exec.
+# -----------------------------
